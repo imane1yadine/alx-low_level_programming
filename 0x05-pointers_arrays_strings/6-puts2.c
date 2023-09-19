5f98eb91b17e8e4dae16ddbabe2e99d624b6a3f1
imane1yadine <jeon37371@gmail.com>
2023-09-18 18:42:13 -0700

    e

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -2,7 +2,7 @@
 
 /**
  * puts2 - Prints every other character of a string,
- * \starting with the first character.
+ * /starting with the first character.
  * @str: A pointer to the string to be printed.
  */
 void puts2(char *str)
