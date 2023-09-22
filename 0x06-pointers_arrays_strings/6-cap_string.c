6dfa911bb293da50c784bee240f90c7d47c228ab
imane1yadine <jeon37371@gmail.com>
2023-09-21 17:16:20 -0700

    a

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,53 +1,15 @@
 #include "main.h"
-#include <stdbool.h>
+#include <stdio.h>
 
-/**
- * is_separator - Check if a character is a word separator.
- * @c: The character to check.
- *
- * Return: true if the character is a separator, false otherwise.
- */
-bool is_separator(char c)
+char *cap_string(char *);
+
+int main(void)
 {
-	char separators[] = " \t\n,;.!?\"(){}";
-	int i;
+	char str[] = "hello world! how are you?";
+	char *ptr;
 
-	for (i = 0; separators[i]; i++)
-	{
-		if (c == separators[i])
-			return (true);
-	}
+	ptr = cap_string(str);
+	printf("%s\n", ptr);
 
-	return (false);
-}
-
-/**
- * cap_string - Capitalize all words in a string.
- * @str: The input string.
- *
- * Return: A pointer to the modified string.
- */
-char *cap_string(char *str)
-{
-	int i;
-	bool new_word = true;
-
-	for (i = 0; str[i]; i++)
-	{
-		if (new_word && islower(str[i]))
-		{
-			str[i] = toupper(str[i]);
-			new_word = false;
-		}
-		else if (is_separator(str[i]))
-		{
-			new_word = true;
-		}
-		else
-		{
-			new_word = false;
-		}
-	}
-
-	return (str);
+	return (0);
 }
