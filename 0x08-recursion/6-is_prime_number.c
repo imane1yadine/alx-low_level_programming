51b43431a7924e722ee31739d77f4f781bf675eb
imane1yadine <jeon37371@gmail.com>
2023-09-28 18:25:40 -0700

    a

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * is_prime_number - Checks if a number is prime.
+ * @n: The number to check.
+ * Return: 1 if n is prime, 0 otherwise.
+ */
+int is_prime_number(int n)
+{
+	if (n <= 1)
+		return (0);
+
+	return (check_prime(n, 2));
+}
+
+/**
+ * check_prime - Helper function to check for primality using recursion.
+ * @n: The number to check for primality.
+ * @divisor: The current divisor to test.
+ * Return: 1 if n is prime, 0 otherwise.
+ */
+int check_prime(int n, int divisor)
+{
+	if (divisor * divisor > n)
+		return (1);
+
+	if (n % divisor == 0)
+		return (0);
+
+	return (check_prime(n, divisor + 1));
+}
