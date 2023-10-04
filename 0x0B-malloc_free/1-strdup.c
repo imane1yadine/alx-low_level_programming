6511520a03ad05a5c1a9faf88545afc0363a9a78
imane1yadine <jeon37371@gmail.com>
2023-10-04 15:30:58 -0700

    a

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -10,21 +10,25 @@
 
 char *_strdup(char *str)
 {
-	if (str == NULL)
-		return (NULL);
-
+	int length;
 	char *duplicate;
-	int length = 0;
 
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+
+	length = 0;
 	while (str[length] != '\0')
 	{
 		length++;
 	}
 
 	duplicate = malloc((length + 1) * sizeof(char));
-
 	if (duplicate == NULL)
+	{
 		return (NULL);
+	}
 
 	for (int i = 0; i <= length; i++)
 	{
