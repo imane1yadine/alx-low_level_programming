ad2acc7f854bb1b403166e80b2eefc9507a76b5b
imane1yadine <jeon37371@gmail.com>
2023-10-08 11:22:42 -0700

    a

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,91 +1,77 @@
 #include <stdlib.h>
-#include <stdio.h>
+#include "main.h"
 
 /**
- * _strlen - Calculate the length of a string.
- * @str: The string to calculate the length of.
+ * count_word - helper function to count the number of words in a string
+ * @s: string to evaluate
  *
- * Return: The length of the string.
+ * Return: number of words
  */
-int _strlen(char *str)
+int count_word(char *s)
 {
-	int length = 0;
+	int flag, c, w;
 
-	while (str[length] != '\0')
-		length++;
+	flag = 0;
+	w = 0;
 
-	return (length);
+	for (c = 0; s[c] != '\0'; c++)
+	{
+		if (s[c] == ' ')
+			flag = 0;
+		else if (flag == 0)
+		{
+			flag = 1;
+			w++;
+		}
+	}
+
+	return (w);
 }
-
 /**
- * _strdup - Duplicate a string.
- * @str: The string to duplicate.
+ * **strtow - splits a string into words
+ * @str: string to split
  *
- * Return: A pointer to the newly allocated duplicated string.
- */
-char *_strdup(char *str)
-{
-	char *duplicate;
-	int i, length;
-
-	if (str == NULL)
-		return (NULL);
-
-	length = _strlen(str);
-
-	duplicate = malloc((length + 1) * sizeof(char));
-
-	if (duplicate == NULL)
-		return (NULL);
-
-	for (i = 0; i < length; i++)
-		duplicate[i] = str[i];
-
-	duplicate[length] = '\0';
-
-	return (duplicate);
-}
-
-/**
- * strtow - Split a string into words.
- * @str: The string to split.
- *
- * Return: A pointer to an array of strings (words), or NULL if it fails.
+ * Return: pointer to an array of strings (Success)
+ * or NULL (Error)
  */
 char **strtow(char *str)
 {
-	char **words;
-	char *token;
-	int i, j, word_count = 0;
+	char **matrix, *tmp;
+	int i, k = 0, len = 0, words, c = 0, start, end;
 
-	if (str == NULL || *str == '\0')
+	while (*(str + len))
+		len++;
+	words = count_word(str);
+	if (words == 0)
 		return (NULL);
 
-	words = malloc(_strlen(str) * sizeof(char *));
-
-	if (words == NULL)
+	matrix = (char **) malloc(sizeof(char *) * (words + 1));
+	if (matrix == NULL)
 		return (NULL);
 
-	token = strtok(str, " ");
-
-	while (token != NULL)
+	for (i = 0; i <= len; i++)
 	{
-		words[word_count] = _strdup(token);
-
-		if (words[word_count] == NULL)
+		if (str[i] == ' ' || str[i] == '\0')
 		{
-			for (i = 0; i < word_count; i++)
-				free(words[i]);
-
-			free(words);
-			return (NULL);
+			if (c)
+			{
+				end = i;
+				tmp = (char *) malloc(sizeof(char) * (c + 1));
+				if (tmp == NULL)
+					return (NULL);
+				while (start < end)
+					*tmp++ = str[start++];
+				*tmp = '\0';
+				matrix[k] = tmp - c;
+				k++;
+				c = 0;
+			}
 		}
-
-		word_count++;
-		token = strtok(NULL, " ");
+		else if (c++ == 0)
+			start = i;
 	}
 
-	words[word_count] = NULL;
+	matrix[k] = NULL;
 
-	return (words);
+	return (matrix);
 }
